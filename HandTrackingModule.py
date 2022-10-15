81479d5b492c278ff8e49b95b651d8e23e9aa680
KroJIak <rastoandrei@gmail.com>
2022-10-15 13:55:39 +0400

    v0.3

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -8,6 +8,7 @@ class handDetector():
         self.model_complexity = model_complexity
         self.detectionCon = detectionCon
         self.trackCon = trackCon
+        self.parentPoint = [-1, 0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19]
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands,
                                         self.model_complexity, self.detectionCon, self.trackCon)
@@ -25,8 +26,14 @@ class handDetector():
         self.getResults()
         if self.results.multi_hand_landmarks:
             if drawAllHand:
-                for handLms in self.results.multi_hand_landmarks:
-                    self.mpDraw.draw_landmarks(self.img, handLms, self.mpHands.HAND_CONNECTIONS)
+                self.pos = self.getPositions()
+                if len(self.pos) == 21:
+                    for point in range(1, 21):
+                        self.prePoint = self.parentPoint[point]
+                        pos1 = [self.pos[self.prePoint][0], self.pos[self.prePoint][1]]
+                        pos2 = [self.pos[point][0], self.pos[point][1]]
+                        self. k = 4
+                        line(self.img, (pos1[0], pos1[1]), (pos2[0], pos2[1]), (255 - point*self.k, 255 - point*self.k, 255 - point*self.k), 3)
             if len(points) != 0:
                 for handLms in self.results.multi_hand_landmarks:
                     for num, pos in enumerate(handLms.landmark):
--- calibration.py
+++ calibration.py
@@ -2,15 +2,23 @@ from HandTrackingModule import handDetector
 from cv2 import *
 from os import listdir
 
+print('Calibrating...')
 detector = handDetector(maxHands=1, detectionCon=0.1)
 files = listdir('alphabet')
-posList = {}
-
-for name in files:
-    img = imread('alphabet/' + name)
-    detector.getImg(img)
-    posList[name[0]] = detector.getPositions()
-
 f = open('resultCalibration.txt', 'w')
-f.write(str(posList))
+f.write('{')
+for word in range(1040, 1072):
+    img = imread('alphabet/' + chr(word) + '.png')
+    detector.getImg(img)
+    s = "'"+chr(word)+"'"+': '+str(detector.getPositions())
+    if word != 1071: s += ', \n'
+    f.write(s)
+    print('Calibrating...             ', int((word-1040) * 100 / 33), '%', sep='')
+print('Calibrating...             100%')
+f.write('}')
 f.close()
+print('Done')
+d = {}
+class n():
+    def __int__(self):
+        self.d = d
